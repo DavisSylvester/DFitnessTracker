94753629a0e92e7f21fd1ed2ea8b42553d8d09a1
Davis Sylvester <davis@sylvesterllc.com>
2018-04-29 03:46:45 -0500

    Added action to navigate to Contact Info

--- app/src/main/java/com/davissylvester/dfitnesstracker/MainActivity.kt
+++ app/src/main/java/com/davissylvester/dfitnesstracker/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.davissylvester.dfitnesstracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showContactInfo(view: View) {
+
+        val intent = Intent(this, trainerContactInfo::class.java)
+
+        startActivity(intent)
     }
 }
