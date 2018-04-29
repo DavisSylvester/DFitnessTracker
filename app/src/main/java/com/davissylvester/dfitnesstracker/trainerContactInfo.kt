c333b05546d0b5aad374286098c122b3e70f3645
Davis Sylvester <davis@sylvesterllc.com>
2018-04-29 17:26:21 -0500

    Added more design info

--- app/src/main/java/com/davissylvester/dfitnesstracker/trainerContactInfo.kt
+++ app/src/main/java/com/davissylvester/dfitnesstracker/trainerContactInfo.kt
@@ -11,12 +11,12 @@ class trainerContactInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trainer_contact_info)
-        setSupportActionBar(toolbar)
+        // setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        //fab.setOnClickListener { view ->
+        //    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //            .setAction("Action", null).show()
+        // }
     }
 
 }
