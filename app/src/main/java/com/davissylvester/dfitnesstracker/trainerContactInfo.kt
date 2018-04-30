ef934981490a0dd03bde6bdb2af92b75d0fd3965
Davis Sylvester <davis@sylvesterllc.com>
2018-04-29 21:50:08 -0500

    Submitting for Grading

--- app/src/main/java/com/davissylvester/dfitnesstracker/trainerContactInfo.kt
+++ app/src/main/java/com/davissylvester/dfitnesstracker/trainerContactInfo.kt
@@ -1,7 +1,7 @@
 package com.davissylvester.dfitnesstracker
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+// import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_trainer_contact_info.*
